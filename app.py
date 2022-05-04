bcf6cd85375f871a541d8e938a83c72b920ced1e
Anzo52 <azollner2014@gmail.com>
2022-05-03 21:56:29 -0600

    Update app.py
    Add flas_selfdoc and annotation. need to test

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import random
 import os
+from flask_selfdoc import Autodoc
 
 app = Flask(__name__)
+auto = Autodoc(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+@auto.doc()
 def index():
     with open('insults.json') as f:
         insults = f.read()
@@ -14,6 +17,9 @@ def index():
     # return in json format
     return insult
 
+@app.route('/documentation')
+def documentation():
+  return auto.html()
 
 if __name__ == '__main__':
     app.run(debug=False)
